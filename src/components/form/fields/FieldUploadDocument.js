06f44567a9350080d6b221c36a36bf408d6468f0
ghomizadeh <front02@simagraphic.com>
2023-04-08 00:59:56 +0330

    updateUploadForm

--- src/components/form/fields/FieldUploadDocument.js
+++ src/components/form/fields/FieldUploadDocument.js
@@ -9,71 +9,25 @@ import { useTranslate } from 'react-admin';
 
 
 function FieldUploadDocument({field}) {
-  const t = useTranslate();
-
-  const {type, kind, size, className, name, label, placeholder, value = [], child, setValue} = field;
-  // console.log('fieldfieldfieldfield', field);
-  if (!Array.isArray(value))
-    return null;
-
-  let [theArray, setArray] = useState(value || []);
-  const addChild = () => {
-    let temp = [...theArray, child];
-
-    setArray(temp);
-  };
-  const removeChild = (id) => {
-    let temp = theArray.filter((i, idx) =>
-      id !== idx
-    );
-
-    setArray(temp);
-  };
+  const t=useTranslate();
+  const {type, kind, size, className, name, label, options, placeholder, value} = field;
+  let [theVal, setTheVal] = useState(value)
 
   return <Col
     sm={size ? size.sm : ''}
     lg={size ? size.lg : ''}
-    className={'MGD arrayField ' + className}>
-
-    <div className='col d-flex justify-content-sb'>
-      <label htmlFor={name}>{label ? t(label) : t(name)}</label>
-      <Button size="small" className='flLeft p-1' onClick={(e) => {
-        e.preventDefault();
-        addChild();
-      }}>
-        <AddIcon/>
-      </Button>
-    </div>
-    <Field
+    className={'MGD ' + className}>
+    <label htmlFor={name}>{label ? t(label) : t(name)}</label>
+    <input
       name={name}
-      className="mb-2 form-control">
-      {() => {
-
-        // return JSON.stringify(theArray);
-        return theArray.map((ch, ke) =>
-          (<div className={'row array-child-' + ke} key={ke}>
-              {child && child.map((ch2, idx2) => {
-                let fieldName = `${name}[${ke}].${ch2.name}`;
-                let Component = getField(ch2.type);
-
-                return <Component key={idx2} field={{
-                  ...ch2,
-                  value: (ch && ch2.name) ? ch[ch2.name] : {},
-                  name: fieldName,
-                  setValue: setValue
-                }}/>;
-
-              })}
-              <span><Button onClick={(e) => {
-                e.preventDefault();
-                removeChild(ke);
-              }}><RemoveCircleOutlineIcon/></Button></span>
-            </div>
-          ));
-
-
+      // component="input"
+      type="file"
+      placeholder={placeholder ? placeholder : (label ? t(label) : t(name))}
+      onChange={(e) => {
+        field.setValue(name, e.target.value)
       }}
-    </Field>
+      className="mb-2 form-control ltr"
+    />
   </Col>
 
 }
