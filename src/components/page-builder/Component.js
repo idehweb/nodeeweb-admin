27e019d5914d0b16f094d83f4a4bbae5f5d17bb2
mohammadghomizadeh <m.ghomizadeh96@gmail.com>
2023-05-29 17:48:11 +0330

    dragDrop

--- src/components/page-builder/Component.js
+++ src/components/page-builder/Component.js
@@ -32,138 +32,230 @@ const Component = (props) => {
   let { fields = {} } = general;
   let { text = "" } = fields;
   const [componentForSetting, setComponentForSetting] = useState(false);
+  const [newComponents, setNewComponents] = useState([]);
   const [{ isDragging }, dragRef] = useDrag({
-        type:  cardType.ORDER,
-        item: { name },
-        collect: (monitor) => ({
-        isDragging:  monitor.isDragging(),
+    type: ItemTypes.KNIGHT,
+    item: { id:component.id },
+    // item: { id:component.id },
+    end:(item,monitor)=>{
+      let res = monitor.getDropResult();
+      if(monitor.getDropResult()){
+        console.log("end item:", item.id, res.id);
+      }
+    },
+    collect: (monitor) => ({
+        isDragging: monitor.isDragging()
     })
+})
+const [{ isOver }, dropRef] = useDrop({
+  accept: ItemTypes.KNIGHT,
+  drop: (item, monitor) => {
+    return ({ id: component.id });
+  },
+  // drop: (item) => setNewComponents((newComponents) => 
+  //   !newComponents.includes(item) ? [...newComponents, item] : newComponents),
+  collect: (monitor) => ({
+      isOver: monitor.isOver()
   })
-
+})
+const dragStart = (e, component) => {
+  console.log("dragStart: ", component.id);
+  // // dragItem.current = position;
+  // if (dragElement == null) {
+  //   dragElement = component.id;
+  //   console.log("dragStart: ", component.id);
+  //   setDragElement(component.id);
+  // }
+};
+    const moveItem = (id,dest) => {
+      console.log('ididididid',id);
+      console.log('destdestdest',dest);
+      let moveCurrentItem = [];
+      let pushCurrentItem = [];
+      let completeComponent = component;
+      // completeComponent.forEach((component)=>{
+        if(component.id === id){
+          moveCurrentItem.push(component);
+        }else if(component.children){
+          component.children.forEach((subCom)=>{
+            if(subCom.id === id){
+              moveCurrentItem.push(subCom);
+            }else if(subCom.children){
+              subCom.children.forEach((subAny)=>{
+                if(subAny.id === id){
+                  moveCurrentItem.push(subAny);
+                }
+              })
+            }
+          })
+        }
+       
+      // })
+      // completeComponent.forEach((component)=>{
+        if(component.id === dest){
+          pushCurrentItem.push(component)
+        }else if(component.children){
+          component.children.forEach((child)=>{
+            if(child.id === dest){
+              pushCurrentItem.push(child)
+            }else if(child.children){
+                child.children.forEach((subChild)=>{
+                  if(subChild.id === dest){
+                    pushCurrentItem.push(subChild)
+                  }
+                })
+            }
+          })
+        }
+       
+      // })
+  if(pushCurrentItem){
+    pushCurrentItem.forEach(push=>{
+      if(push.hasOwnProperty('children')){
+        push.children.push(moveCurrentItem[0]);
+        component.splice(component.findIndex(a => a.id === id) , 1)
+      }else{
+        Object.assign(push,{children:moveCurrentItem})
+        component.splice(component.findIndex(a => a.id === id) , 1)
+      }
+    })
+  
+  }
+ 
+      // setState({ ...state, components: component });
+    };
   return (
-    <Fragment>
-      <div className={"nodeweb-element-wrapper"} id={component.id}>
-        <div className={'element-header'}>
-            <span>Element:&nbsp;&nbsp; {component.name + " " + (index + 1)}</span>
-        </div>
-        <div className={'controller'}>
-          <span className={'move'} style={{border:'1px solid #ddd',padding:'1px 3px',cursor:'grab'}}>
-            <MoveIconSvg width="15px" height="15px" background="#464D55"/>
-          </span>
-          <span
-          onClick={()=>setComponentForSetting(!componentForSetting)}
-           className={'edit'} style={{border:'1px solid #ddd',borderTop:'none',padding:'1px 3px',cursor:'pointer'}}>
-            <EditIconSvg width="15px" height="15px" fill="none"  background="#464D55"/>
-          </span>
-        </div>
-        <div className={'content'}>
-        {!componentForSetting && <div
-        >
-
-          {component.addable && <div className={"add-part p-2 name-" + component.name}>
-            <div className={"element-wrapper-child"}>
-              {Boolean(component.children && (component.children instanceof Array)) && component.children.map((comp, jj) => {
-                
-                return (<Component
-                  key={jj}
-                  index={jj}
-                  component={comp}
-                  // moveItem={moveItem}
-                  // moveContent={moveContent}
-                  setComponentForSetting={setComponentForSetting}
-                  toggleComponentOptionsBox={toggleComponentOptionsBox}
-                  setExcludeArray={() => setExcludeArray([])}
-                  changeComponentSetting={(e, j, d) => changeComponentSetting(e, j, d)}
-                  deleteItem={(e) => {
-                    setComponentForSetting(false);
-                    deleteItem(e);
-                  }}
-                  toggleOptionBox={toggleOptionBox}
-                  length={component.children.length}
-                  address={component.id + "_" + jj}
-                  child={true}
-                />);
-              })}
+    <Fragment >
+          <div className={"nodeweb-element-wrapper"} id={component.id} ref={dragRef} draggable   >
+          
+            <div className={index > 2 ? 'mtop-10 element-header' : 'element-header'} >
+                <span>Element:&nbsp;&nbsp; {component.name + " " + (index + 1)} CP:{component.id}</span>
             </div>
-          </div>}
+            <div className={'controller'}>
+              <span className={'move'} style={{border:'1px solid #ddd',padding:'1px 3px',cursor:'grab'}}
+              
+                draggable
 
+                onDragStart={(e) => dragStart(e, component)}
+                onDragEnd={(e) => {
+                  console.log('on drop on label:',component.id)
+                  onDrop(component);
+                }}
+                onDragEnter={(e) => dragEnter(e, component)}
+              >
+                <MoveIconSvg width="15px" height="15px" background="#464D55"/>
+              </span>
+              <span
+              onClick={()=>setComponentForSetting(!componentForSetting)}
+              className={'edit'} style={{border:'1px solid #ddd',borderTop:'none',padding:'1px 3px',cursor:'pointer'}}>
+                <EditIconSvg width="15px" height="15px" fill="none"  background="#464D55"/>
+              </span>
+            </div>
+            <div className={'content'}>
+            {!componentForSetting && <div
+            >
 
+              {component.addable && <div className={"add-part p-2 name-" + component.name} >
+                <div className={"element-wrapper-child"} ref={dropRef} id={component.id}>
+                  {Boolean(component.children && (component.children instanceof Array)) && component.children.map((comp, jj) => {
+                    
+                    return (<Component
+                      key={jj}
+                      index={jj}
+                      component={comp}
+                      // moveItem={moveItem}
+                      // moveContent={moveContent}
+                      setComponentForSetting={setComponentForSetting}
+                      toggleComponentOptionsBox={toggleComponentOptionsBox}
+                      setExcludeArray={() => setExcludeArray([])}
+                      changeComponentSetting={(e, j, d) => changeComponentSetting(e, j, d)}
+                      deleteItem={(e) => {
+                        setComponentForSetting(false);
+                        deleteItem(e);
+                      }}
+                      toggleOptionBox={toggleOptionBox}
+                      length={component.children.length}
+                      address={component.id + "_" + jj}
+                      child={true}
+                    />);
+                  })}
+                  
+                </div>
+              </div>}
+              {isOver && child && <div style={{width:'100%',height:'40px',border:'1px solid #ddd',background:'#ddd'}}></div>}
+            </div>}
+            
+            {/*StartModalSetting*/}
+            {componentForSetting && <div draggable={true} className={"component-set-for-setting"} >
+              <div className={"csfs-a"}>
+                <div className={"csfs-c"}>
+                  <div className={"top-bar-settings"}>
+                    <Button className={"closeIcon"} onClick={() => {
+                      setComponentForSetting(!componentForSetting);
+                      }}><CloseIcon/>
+                    </Button>
+                    <Button
+                      className={"redxxx"}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setComponentForSetting(false);
+                        deleteItem(component.id);
+                      }}>
+                      <DeleteForeverIcon/>{("Delete")}
+                    </Button>
+                  </div>
+                  <div className={"bottom-bar-settings"} >
+                    {/*{JSON.stringify(component.settings.general.fields)}*/}
+                    {component.settings && component.settings.general && <CreateForm
+                      onSubmit={(e) => {
+                        changeComponentSetting(component, "general", e);
+                        setComponentForSetting(!componentForSetting);
+                      }}
+                      rules={{ fields: component.settings.general.rules }}
+                      buttons={[]}
+                      componentType={component.name}
 
-
-         
-        </div>}
-        {/*StartModalSetting*/}
-        {componentForSetting && <div draggable={true} className={"component-set-for-setting"} >
-          <div className={"csfs-a"}>
-            <div className={"csfs-c"}>
-              <div className={"top-bar-settings"}>
-                <Button className={"closeIcon"} onClick={() => {
-                  setComponentForSetting(!componentForSetting);
-                  }}><CloseIcon/>
-                </Button>
-                <Button
-                  className={"redxxx"}
+                      fields={component.settings.general.fields}/>}
+                  </div>
+                </div>
+              </div>
+            </div>}
+            </div>
+            <div className={'element-footer'}>
+            <div>
+              <span style={{cursor:'pointer'}}
+                onClick={(e) => {
+                  e.preventDefault();
+                  setComponentForSetting(false);
+                  deleteItem(component.id);
+                }}
+              ><CloseIconSvg width="20px" height="20px"  background={'#464D55'}/></span>
+              <span style={{margin:'0px 10px',cursor:'pointer'}}
+                onClick={()=>setComponentForSetting(!componentForSetting)}
+              
+              ><EditIconSvg width="15px" height="15px" background={'#464D55'}/></span>
+              {
+                component.addable && (
+                  <span style={{cursor:'pointer'}}
                   onClick={(e) => {
-                    e.preventDefault();
-                    setComponentForSetting(false);
-                    deleteItem(component.id);
-                  }}>
-                  <DeleteForeverIcon/>{("Delete")}
-                </Button>
-              </div>
-              <div className={"bottom-bar-settings"} >
-                {/*{JSON.stringify(component.settings.general.fields)}*/}
-                {component.settings && component.settings.general && <CreateForm
-                  onSubmit={(e) => {
-                    changeComponentSetting(component, "general", e);
-                    setComponentForSetting(!componentForSetting);
+                    let address = component.id + "_";
+                    let mainAddress = address.split("_");
+                    let update = { sourceAddress: component.id };
+                    if (mainAddress[4]) {
+                      update["excludeArray"] = ["row"];
+                    } else {
+                      update["excludeArray"] = [];
+                    }
+                    toggleOptionBox(update);
                   }}
-                  rules={{ fields: component.settings.general.rules }}
-                  buttons={[]}
-                  componentType={component.name}
-
-                  fields={component.settings.general.fields}/>}
-              </div>
+                  
+                  ><AddIconSvg width="15px" height="15px" fill="#464D55"/></span>
+                )
+              }
+            
+            </div>
             </div>
           </div>
-        </div>}
-        </div>
-        <div className={'element-footer'}>
-        <div>
-          <span style={{cursor:'pointer'}}
-            onClick={(e) => {
-              e.preventDefault();
-              setComponentForSetting(false);
-              deleteItem(component.id);
-            }}
-          ><CloseIconSvg width="20px" height="20px"  background={'#464D55'}/></span>
-          <span style={{margin:'0px 10px',cursor:'pointer'}}
-            onClick={()=>setComponentForSetting(!componentForSetting)}
-          
-          ><EditIconSvg width="15px" height="15px" background={'#464D55'}/></span>
-          {
-            component.addable && (
-              <span style={{cursor:'pointer'}}
-              onClick={(e) => {
-                let address = component.id + "_";
-                let mainAddress = address.split("_");
-                let update = { sourceAddress: component.id };
-                if (mainAddress[4]) {
-                  update["excludeArray"] = ["row"];
-                } else {
-                  update["excludeArray"] = [];
-                }
-                toggleOptionBox(update);
-              }}
-              
-              ><AddIconSvg width="15px" height="15px" fill="#464D55"/></span>
-            )
-          }
-         
-        </div>
-        </div>
-      </div>
-      
     </Fragment>
   );
 };
